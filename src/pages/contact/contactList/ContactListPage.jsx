4add1256b106467a6ebf9f9b7ac01840f97ba7b6
bharatmer <bharatmer9@gmail.com>
2025-04-20 13:20:32 +0530

    Working on contact

--- src/pages/contact/contactList/ContactListPage.jsx
+++ src/pages/contact/contactList/ContactListPage.jsx
@@ -38,8 +38,15 @@ const ContactListPage = () => {
   return (
     <Fragment>
       <Container>
-        <div className="flex justify-between items-center mb-4">
-          <div className="flex items-center border border-gray-300 rounded-lg overflow-hidden">
+        <div className="flex flex-wrap items-center lg:items-end justify-between gap-2 mb-3">
+          <div className="relative mb-1">
+           <i className="ki-filled ki-magnifier leading-none text-md text-gray-500 absolute top-1/2 start-0 -translate-y-1/2 ms-3"></i>
+           <input 
+            className="input input-sm pl-8"
+            placeholder="Example input"
+            type="text" />
+          </div>
+          {/* <div className="flex items-center border border-gray-300 rounded-lg overflow-hidden">
             <span className="px-3 bg-gray-100">
               <KeenIcon icon="magnifier" className="text-gray-700 text-xl" />
             </span>
@@ -48,9 +55,9 @@ const ContactListPage = () => {
               placeholder="Example input"
               type="text"
             />
-          </div>
-          <button className="btn btn-primary" onClick={handleModalOpen}>
-            Add Contact
+          </div> */}
+          <button className="btn btn-sm btn-light mb-1" onClick={handleModalOpen}>
+            + Add Contact
           </button>
         </div>
         <TableComponent columns={columns} data={tableData} />
