e32077e40f036f5ed200147b0935f7c70b16ed82
bharatmer <bharatmer9@gmail.com>
2025-04-26 19:16:18 +0530

    Set tabs

--- src/partials/modals/add-company/AddCompany.jsx
+++ src/partials/modals/add-company/AddCompany.jsx
@@ -29,94 +29,92 @@ const AddCompany = ({ isModalOpen, setIsModalOpen }) => {
             Save Company
           </button>,
         ]}
-      >
-       
+      >       
        <div className="grid grid-cols-2 gap-6">
-              <div>
-                <label className="form-label ">Company Name</label>
-                <input
-                  type="text"
-                  className="input form-control-solid w-full"
-                  placeholder="Company Name"
-                />
-              </div>
-              <div>
-                <label className="form-label ">Tax No</label>
-                <input
-                  type="text"
-                  className="input form-control-solid w-full"
-                  placeholder="Tax No"
-                />
-              </div>
-              <div>
-                <label className="form-label ">Company Code</label>
-                <input
-                  type="text"
-                  className="input form-control-solid w-full"
-                  placeholder="Company Code"
-                />
-              </div>
-              <div>
-                <label className="form-label ">Website</label>
-                <input
-                  type="text"
-                  className="input form-control-solid w-full"
-                  placeholder="Enter URL"
-                />
-              </div>
-            </div>
+          <div>
+            <label className="form-label ">Company Name</label>
+            <input
+              type="text"
+              className="input form-control-solid w-full"
+              placeholder="Company Name"
+            />
+          </div>
+          <div>
+            <label className="form-label ">Tax No</label>
+            <input
+              type="text"
+              className="input form-control-solid w-full"
+              placeholder="Tax No"
+            />
+          </div>
+          <div>
+            <label className="form-label ">Company Code</label>
+            <input
+              type="text"
+              className="input form-control-solid w-full"
+              placeholder="Company Code"
+            />
+          </div>
+          <div>
+            <label className="form-label ">Website</label>
+            <input
+              type="text"
+              className="input form-control-solid w-full"
+              placeholder="Enter URL"
+            />
+          </div>
+        </div>
        <hr />
-
        <div className="grid grid-cols-2 gap-6">
-              <div>
-                <label className="form-label ">Country</label>
-                <input
-                  type="text"
-                  className="input form-control-solid w-full"
-                  placeholder="Country"
-                />
-              </div>
-              <div>
-                <label className="form-label ">State</label>
-                <input
-                  type="text"
-                  className="input form-control-solid w-full"
-                  placeholder="State"
-                />
-              </div>
-              <div>
-                <label className="form-label ">City</label>
-                <input
-                  type="text"
-                  className="input form-control-solid w-full"
-                  placeholder="City"
-                />
-              </div>
-              <div>
-                <label className="form-label ">Pincode</label>
-                <input
-                  type="tel"
-                  className="input form-control-solid w-full"
-                  placeholder="Pincode"
-                />
-              </div>
-              <div>
-                <label className="form-label ">Billing Address</label>
-                <input
-                  type="text"
-                  className="input form-control-solid w-full"
-                  placeholder="Billing Address"
-                />
-              </div>
-              <div>
-                <label className="form-label ">Shipping Address</label>
-                <input
-                  type="text"
-                  className="input form-control-solid w-full"
-                  placeholder="Shipping Address"
-                />
-              </div>
-            </div>
+          <div>
+            <label className="form-label ">Country</label>
+            <input
+              type="text"
+              className="input form-control-solid w-full"
+              placeholder="Country"
+            />
+          </div>
+          <div>
+            <label className="form-label ">State</label>
+            <input
+              type="text"
+              className="input form-control-solid w-full"
+              placeholder="State"
+            />
+          </div>
+          <div>
+            <label className="form-label ">City</label>
+            <input
+              type="text"
+              className="input form-control-solid w-full"
+              placeholder="City"
+            />
+          </div>
+          <div>
+            <label className="form-label ">Pincode</label>
+            <input
+              type="tel"
+              className="input form-control-solid w-full"
+              placeholder="Pincode"
+            />
+          </div>
+          <div>
+            <label className="form-label ">Billing Address</label>
+            <input
+              type="text"
+              className="input form-control-solid w-full"
+              placeholder="Billing Address"
+            />
+          </div>
+          <div>
+            <label className="form-label ">Shipping Address</label>
+            <input
+              type="text"
+              className="input form-control-solid w-full"
+              placeholder="Shipping Address"
+            />
+          </div>
+        </div>
        
       </CustomModal>
     )
