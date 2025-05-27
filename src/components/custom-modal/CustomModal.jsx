6c21f38079b29cbd8fef4280aee6fb0195baef8c
kaushik-kboy <kaushikbhrahmbhatt@gmail.com>
2025-05-27 23:19:37 +0530

    Antd added and antd modal converted

--- src/components/custom-modal/CustomModal.jsx
+++ src/components/custom-modal/CustomModal.jsx
@@ -1,13 +1,8 @@
-import { useState } from "react"; // Add this
-import { IconButton, Typography } from "@mui/material";
-import { X } from "lucide-react";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogTitle from "@mui/material/DialogTitle";
-import DialogContent from "@mui/material/DialogContent";
+import { useState } from "react";
+import { Modal } from "antd";
 import useStyle from "./style";
 
-const CustomModal = ({ open, onClose, children, title, footer, width }) => {
+const CustomModal = ({ open, onClose, children, title, width, ...rest }) => {
   const classes = useStyle();
   const [shake, setShake] = useState(false);
 
@@ -20,44 +15,16 @@ const CustomModal = ({ open, onClose, children, title, footer, width }) => {
     onClose();
   };
   return (
-    <Dialog
-      className={`${classes.customModal} ${shake ? classes.shake : ""}`}
+    <Modal
+      title={title}
+      closable={{ "aria-label": "Custom Close Button" }}
       open={open}
-      onClose={handleClose}
-      scroll={"body"}
-      aria-labelledby="scroll-dialog-title"
-      aria-describedby="scroll-dialog-description"
-      fullWidth={!!width === false}
-      maxWidth={false}
-      PaperProps={{
-        sx: {
-          width: width || "600px",
-          maxWidth: "100%",
-        },
-      }}
+      onCancel={handleClose}
+      {...rest}
     >
-      <DialogTitle id="scroll-dialog-title">
-        <Typography variant="h6">{title}</Typography>
-        <IconButton
-          aria-label="close"
-          onClick={onClose}
-          sx={{
-            position: "absolute",
-            right: 8,
-            top: 8,
-            color: (theme) => theme.palette.grey[500],
-          }}
-        >
-          <X />
-        </IconButton>
-      </DialogTitle>
-      <DialogContent>{children}</DialogContent>
-      <DialogActions>
-        {footer.map((item, index) => {
-          return item;
-        })}
-      </DialogActions>
-    </Dialog>
+      {children}
+      <div className="flex justify-end modal-footer"></div>
+    </Modal>
   );
 };
 export { CustomModal };
