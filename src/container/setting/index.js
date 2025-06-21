d5a78054d1d038255e870fac84867a8b45df4070
rahulgohel20-jw <rahul.shree0101@gmail.com>
2025-06-21 14:47:52 +0530

    All Attendance and Setting Leave Type

--- src/container/setting/index.js
+++ src/container/setting/index.js
@@ -7,3 +7,7 @@ export * from "./Customize/ContactTab";
 export * from "./General_Setting/RoleAndPermission";
 export * from "./General_Setting/BulkDataImport";
 export * from "./General_Setting/ExportLead";
+
+
+export * from "./AllAttendance/Attendance"
+export * from "./AllAttendance/Regularization"
--- src/routing/AppRoutingSetup.jsx
+++ src/routing/AppRoutingSetup.jsx
@@ -110,10 +110,12 @@ import {
 } from "@/pages/tasks";
 
 import { Holiday } from "@/pages/Leave/holiday";
-import { MyLeaves } from "../pages/Leave/my-leaves";
-import {Approval} from "../pages/Leave/approval";
+import { MyLeaves } from "@/pages/Leave/my-leaves";
+import {Approval} from "@/pages/Leave/approval";
 
 import Allleave from "@/pages/Leave/all-leave/Allleave";
+import { AllAttendance } from "@/pages/Leave/all-attendance";
+import { LeaveType } from "@/pages/Leave/settings/leave-type/LeaveType";
 
 
 const AppRoutingSetup = () => {
@@ -155,7 +157,8 @@ const AppRoutingSetup = () => {
 <Route path="/holiday" element={<Holiday/>}></Route>
 <Route path="/myleaves" element={<MyLeaves/>}></Route>
 <Route path="/allleave" element={<Allleave/>}></Route>
-
+<Route path="allattendance" element={<AllAttendance/>}></Route>
+<Route path="leavetype" element={<LeaveType/>}></Route>
 
 
 
