e4740152494f7af2d52e3b54b698b27df6f5e5c3
bharatmer <bharatmer9@gmail.com>
2025-07-07 00:15:48 +0530

    Manage Tasks module is done

--- src/container/tasks/AllTasks.jsx
+++ src/container/tasks/AllTasks.jsx
@@ -33,6 +33,264 @@ const AllTasks = () => {
             }
           `}
         </style>
+         <div className="card min-w-full">
+          <div className="flex items-start border-t first:border-0 border-gray-200 px-4 pt-3 even:bg-gray-100">
+            <div className="flex flex-col w-full gap-1.5">
+              <div className="flex items-center w-full gap-2 cursor-pointer">
+                <div className="flex flex-no-wrap text-md font-normal text-gray-700">TASK-{item.id}</div>
+                <div className="text-md font-medium text-primary">Task title Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took</div>
+              </div>
+              <div className="text-sm text-gray-700 mb-1">Description It was popularised in the 1960s with the release of Letraset sheets containing.</div>
+              <div className="flex items-center gap-4">
+                <div className="flex justify-between items-start gap-1 h-[24px] cursor-pointer" title="User">
+                  <span className="text-xs font-medium text-primary"><i className="ki-filled ki-user"></i> User name</span>
+                </div>
+                <div className="flex items-start gap-1 h-[24px] cursor-pointer" title="Date">
+                  <span className="text-xs font-medium text-gray-700"><i className="ki-filled ki-calendar-2"></i> July 26 2025</span>
+                </div>
+                <div className="flex items-start gap-1 h-[24px] cursor-pointer" title="Category">
+                  <span className="text-xs font-medium text-gray-700"><i className="ki-filled ki-bookmark"></i> Category name</span>
+                </div>
+                <div className="flex flex-col justify-between items-start gap-1 h-[24px] w-[80px] cursor-pointer" title="Priority">
+                  <span className="text-xs font-medium text-gray-700">High</span>
+                  <span className="w-full h-[4px] bg-danger rounded-t"></span>
+                </div>
+                <div className="flex flex-col justify-between items-start gap-1 h-[24px] w-[80px] cursor-pointer" title="Status">
+                  <span className="text-xs font-medium text-gray-700">In progress</span>
+                  <span className="w-full h-[4px] bg-success rounded-t"></span>
+                </div>
+                <div className="flex flex-col justify-between items-start gap-1 h-[24px] w-[80px] cursor-pointer" title="Repeat">
+                  <span className="text-xs font-medium text-gray-700">Daily</span>
+                  <span className="w-full h-[4px] bg-info rounded-t"></span>
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center gap-3.5 ms-auto">
+              <div className="flex items-center gap-3.5 ms-auto">
+                <span className="flex items-center gap-1 cursor-pointer">
+                  <i className="ki-filled ki-messages text-gray-600 hover:text-primary"></i>
+                  <span className="text-sm">3</span>
+                </span>
+                <span className="cursor-pointer"><i className="ki-filled ki-like text-gray-600 hover:text-primary"></i></span>
+              </div>
+              <button className="btn btn-sm btn-icon btn-light btn-clear" title="Expand"><i className="ki-filled ki-up"></i></button>
+            </div>
+          </div>
+          <div className="flex items-start border-t first:border-0 border-gray-200 px-4 pt-3 even:bg-gray-100">
+            <div className="flex flex-col w-full gap-1.5">
+              <div className="flex items-center w-full gap-2 cursor-pointer">
+                <div className="flex flex-no-wrap text-md font-normal text-gray-700">TASK-{item.id}</div>
+                <div className="text-md font-medium text-primary">Task title Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took</div>
+              </div>
+              {/* <di className="text-sm text-gray-700 mb-1">Description It was popularised in the 1960s with the release of Letraset sheets containing.</div> */}
+              <div className="flex items-center gap-4">
+                <div className="flex justify-between items-start gap-1 h-[24px] cursor-pointer" title="User">
+                  <span className="text-xs font-medium text-primary"><i className="ki-filled ki-user"></i> User name</span>
+                </div>
+                <div className="flex items-start gap-1 h-[24px] cursor-pointer" title="Date">
+                  <span className="text-xs font-medium text-gray-700"><i className="ki-filled ki-calendar-2"></i> July 26 2025</span>
+                </div>
+                <div className="flex items-start gap-1 h-[24px] cursor-pointer" title="Category">
+                  <span className="text-xs font-medium text-gray-700"><i className="ki-filled ki-bookmark"></i> Category name</span>
+                </div>
+                <div className="flex flex-col justify-between items-start gap-1 h-[24px] w-[80px] cursor-pointer" title="Priority">
+                  <span className="text-xs font-medium text-gray-700">High</span>
+                  <span className="w-full h-[4px] bg-danger rounded-t"></span>
+                </div>
+                <div className="flex flex-col justify-between items-start gap-1 h-[24px] w-[80px] cursor-pointer" title="Status">
+                  <span className="text-xs font-medium text-gray-700">In progress</span>
+                  <span className="w-full h-[4px] bg-success rounded-t"></span>
+                </div>
+                <div className="flex flex-col justify-between items-start gap-1 h-[24px] w-[80px] cursor-pointer" title="Repeat">
+                  <span className="text-xs font-medium text-gray-700">Daily</span>
+                  <span className="w-full h-[4px] bg-info rounded-t"></span>
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center gap-3.5 ms-auto">
+              <div className="flex items-center gap-3.5 ms-auto">
+                <span className="flex items-center gap-1 cursor-pointer">
+                  <i className="ki-filled ki-messages text-gray-600 hover:text-primary"></i>
+                  <span className="text-sm">3</span>
+                </span>
+                <span className="cursor-pointer"><i className="ki-filled ki-like text-gray-600 hover:text-primary"></i></span>
+              </div>
+              <button className="btn btn-sm btn-icon btn-light btn-clear" title="Expand"><i className="ki-filled ki-down"></i></button>
+            </div>
+          </div>
+          <div className="flex items-start border-t first:border-0 border-gray-200 px-4 pt-3 even:bg-gray-100">
+            <div className="flex flex-col w-full gap-1.5">
+              <div className="flex items-center w-full gap-2 cursor-pointer">
+                <div className="flex flex-no-wrap text-md font-normal text-gray-700">TASK-{item.id}</div>
+                <div className="text-md font-medium text-primary">Task title Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took</div>
+              </div>
+              {/* <di className="text-sm text-gray-700 mb-1">Description It was popularised in the 1960s with the release of Letraset sheets containing.</div> */}
+              <div className="flex items-center gap-4">
+                <div className="flex justify-between items-start gap-1 h-[24px] cursor-pointer" title="User">
+                  <span className="text-xs font-medium text-primary"><i className="ki-filled ki-user"></i> User name</span>
+                </div>
+                <div className="flex items-start gap-1 h-[24px] cursor-pointer" title="Date">
+                  <span className="text-xs font-medium text-gray-700"><i className="ki-filled ki-calendar-2"></i> July 26 2025</span>
+                </div>
+                <div className="flex items-start gap-1 h-[24px] cursor-pointer" title="Category">
+                  <span className="text-xs font-medium text-gray-700"><i className="ki-filled ki-bookmark"></i> Category name</span>
+                </div>
+                <div className="flex flex-col justify-between items-start gap-1 h-[24px] w-[80px] cursor-pointer" title="Priority">
+                  <span className="text-xs font-medium text-gray-700">High</span>
+                  <span className="w-full h-[4px] bg-danger rounded-t"></span>
+                </div>
+                <div className="flex flex-col justify-between items-start gap-1 h-[24px] w-[80px] cursor-pointer" title="Status">
+                  <span className="text-xs font-medium text-gray-700">In progress</span>
+                  <span className="w-full h-[4px] bg-success rounded-t"></span>
+                </div>
+                <div className="flex flex-col justify-between items-start gap-1 h-[24px] w-[80px] cursor-pointer" title="Repeat">
+                  <span className="text-xs font-medium text-gray-700">Daily</span>
+                  <span className="w-full h-[4px] bg-info rounded-t"></span>
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center gap-3.5 ms-auto">
+              <div className="flex items-center gap-3.5 ms-auto">
+                <span className="flex items-center gap-1 cursor-pointer">
+                  <i className="ki-filled ki-messages text-gray-600 hover:text-primary"></i>
+                  <span className="text-sm">3</span>
+                </span>
+                <span className="cursor-pointer"><i className="ki-filled ki-like text-gray-600 hover:text-primary"></i></span>
+              </div>
+              <button className="btn btn-sm btn-icon btn-light btn-clear" title="Expand"><i className="ki-filled ki-down"></i></button>
+            </div>
+          </div>
+          <div className="flex items-start border-t first:border-0 border-gray-200 px-4 pt-3 even:bg-gray-100">
+            <div className="flex flex-col w-full gap-1.5">
+              <div className="flex items-center w-full gap-2 cursor-pointer">
+                <div className="flex flex-no-wrap text-md font-normal text-gray-700">TASK-{item.id}</div>
+                <div className="text-md font-medium text-primary">Task title Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took</div>
+              </div>
+              {/* <di className="text-sm text-gray-700 mb-1">Description It was popularised in the 1960s with the release of Letraset sheets containing.</div> */}
+              <div className="flex items-center gap-4">
+                <div className="flex justify-between items-start gap-1 h-[24px] cursor-pointer" title="User">
+                  <span className="text-xs font-medium text-primary"><i className="ki-filled ki-user"></i> User name</span>
+                </div>
+                <div className="flex items-start gap-1 h-[24px] cursor-pointer" title="Date">
+                  <span className="text-xs font-medium text-gray-700"><i className="ki-filled ki-calendar-2"></i> July 26 2025</span>
+                </div>
+                <div className="flex items-start gap-1 h-[24px] cursor-pointer" title="Category">
+                  <span className="text-xs font-medium text-gray-700"><i className="ki-filled ki-bookmark"></i> Category name</span>
+                </div>
+                <div className="flex flex-col justify-between items-start gap-1 h-[24px] w-[80px] cursor-pointer" title="Priority">
+                  <span className="text-xs font-medium text-gray-700">High</span>
+                  <span className="w-full h-[4px] bg-danger rounded-t"></span>
+                </div>
+                <div className="flex flex-col justify-between items-start gap-1 h-[24px] w-[80px] cursor-pointer" title="Status">
+                  <span className="text-xs font-medium text-gray-700">In progress</span>
+                  <span className="w-full h-[4px] bg-success rounded-t"></span>
+                </div>
+                <div className="flex flex-col justify-between items-start gap-1 h-[24px] w-[80px] cursor-pointer" title="Repeat">
+                  <span className="text-xs font-medium text-gray-700">Daily</span>
+                  <span className="w-full h-[4px] bg-info rounded-t"></span>
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center gap-3.5 ms-auto">
+              <div className="flex items-center gap-3.5 ms-auto">
+                <span className="flex items-center gap-1 cursor-pointer">
+                  <i className="ki-filled ki-messages text-gray-600 hover:text-primary"></i>
+                  <span className="text-sm">3</span>
+                </span>
+                <span className="cursor-pointer"><i className="ki-filled ki-like text-gray-600 hover:text-primary"></i></span>
+              </div>
+              <button className="btn btn-sm btn-icon btn-light btn-clear" title="Expand"><i className="ki-filled ki-down"></i></button>
+            </div>
+          </div>
+          <div className="flex items-start border-t first:border-0 border-gray-200 px-4 pt-3 even:bg-gray-100">
+            <div className="flex flex-col w-full gap-1.5">
+              <div className="flex items-center w-full gap-2 cursor-pointer">
+                <div className="flex flex-no-wrap text-md font-normal text-gray-700">TASK-{item.id}</div>
+                <div className="text-md font-medium text-primary">Task title Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took</div>
+              </div>
+              {/* <di className="text-sm text-gray-700 mb-1">Description It was popularised in the 1960s with the release of Letraset sheets containing.</div> */}
+              <div className="flex items-center gap-4">
+                <div className="flex justify-between items-start gap-1 h-[24px] cursor-pointer" title="User">
+                  <span className="text-xs font-medium text-primary"><i className="ki-filled ki-user"></i> User name</span>
+                </div>
+                <div className="flex items-start gap-1 h-[24px] cursor-pointer" title="Date">
+                  <span className="text-xs font-medium text-gray-700"><i className="ki-filled ki-calendar-2"></i> July 26 2025</span>
+                </div>
+                <div className="flex items-start gap-1 h-[24px] cursor-pointer" title="Category">
+                  <span className="text-xs font-medium text-gray-700"><i className="ki-filled ki-bookmark"></i> Category name</span>
+                </div>
+                <div className="flex flex-col justify-between items-start gap-1 h-[24px] w-[80px] cursor-pointer" title="Priority">
+                  <span className="text-xs font-medium text-gray-700">High</span>
+                  <span className="w-full h-[4px] bg-danger rounded-t"></span>
+                </div>
+                <div className="flex flex-col justify-between items-start gap-1 h-[24px] w-[80px] cursor-pointer" title="Status">
+                  <span className="text-xs font-medium text-gray-700">In progress</span>
+                  <span className="w-full h-[4px] bg-success rounded-t"></span>
+                </div>
+                <div className="flex flex-col justify-between items-start gap-1 h-[24px] w-[80px] cursor-pointer" title="Repeat">
+                  <span className="text-xs font-medium text-gray-700">Daily</span>
+                  <span className="w-full h-[4px] bg-info rounded-t"></span>
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center gap-3.5 ms-auto">
+              <div className="flex items-center gap-3.5 ms-auto">
+                <span className="flex items-center gap-1 cursor-pointer">
+                  <i className="ki-filled ki-messages text-gray-600 hover:text-primary"></i>
+                  <span className="text-sm">3</span>
+                </span>
+                <span className="cursor-pointer"><i className="ki-filled ki-like text-gray-600 hover:text-primary"></i></span>
+              </div>
+              <button className="btn btn-sm btn-icon btn-light btn-clear" title="Expand"><i className="ki-filled ki-down"></i></button>
+            </div>
+          </div>
+          <div className="flex items-start border-t first:border-0 border-gray-200 px-4 pt-3 even:bg-gray-100">
+            <div className="flex flex-col w-full gap-1.5">
+              <div className="flex items-center w-full gap-2 cursor-pointer">
+                <div className="flex flex-no-wrap text-md font-normal text-gray-700">TASK-{item.id}</div>
+                <div className="text-md font-medium text-primary">Task title Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took</div>
+              </div>
+              {/* <di className="text-sm text-gray-700 mb-1">Description It was popularised in the 1960s with the release of Letraset sheets containing.</div> */}
+              <div className="flex items-center gap-4">
+                <div className="flex justify-between items-start gap-1 h-[24px] cursor-pointer" title="User">
+                  <span className="text-xs font-medium text-primary"><i className="ki-filled ki-user"></i> User name</span>
+                </div>
+                <div className="flex items-start gap-1 h-[24px] cursor-pointer" title="Date">
+                  <span className="text-xs font-medium text-gray-700"><i className="ki-filled ki-calendar-2"></i> July 26 2025</span>
+                </div>
+                <div className="flex items-start gap-1 h-[24px] cursor-pointer" title="Category">
+                  <span className="text-xs font-medium text-gray-700"><i className="ki-filled ki-bookmark"></i> Category name</span>
+                </div>
+                <div className="flex flex-col justify-between items-start gap-1 h-[24px] w-[80px] cursor-pointer" title="Priority">
+                  <span className="text-xs font-medium text-gray-700">High</span>
+                  <span className="w-full h-[4px] bg-danger rounded-t"></span>
+                </div>
+                <div className="flex flex-col justify-between items-start gap-1 h-[24px] w-[80px] cursor-pointer" title="Status">
+                  <span className="text-xs font-medium text-gray-700">In progress</span>
+                  <span className="w-full h-[4px] bg-success rounded-t"></span>
+                </div>
+                <div className="flex flex-col justify-between items-start gap-1 h-[24px] w-[80px] cursor-pointer" title="Repeat">
+                  <span className="text-xs font-medium text-gray-700">Daily</span>
+                  <span className="w-full h-[4px] bg-info rounded-t"></span>
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center gap-3.5 ms-auto">
+              <div className="flex items-center gap-3.5 ms-auto">
+                <span className="flex items-center gap-1 cursor-pointer">
+                  <i className="ki-filled ki-messages text-gray-600 hover:text-primary"></i>
+                  <span className="text-sm">3</span>
+                </span>
+                <span className="cursor-pointer"><i className="ki-filled ki-like text-gray-600 hover:text-primary"></i></span>
+              </div>
+              <button className="btn btn-sm btn-icon btn-light btn-clear" title="Expand"><i className="ki-filled ki-down"></i></button>
+            </div>
+          </div>
+        </div>
+        
+        <div className="my-7"></div>
+
+
         <div className="flex flex-col gap-3 lg:gap-4">
           <div className="card min-w-full">
             <div className="flex flex-col flex-1">
