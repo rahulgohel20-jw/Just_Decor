bb0c02c24ecc00edd193e243f4bd2f95928e474f
Yatin Patel <patelyatin604@gmail.com>
2025-08-07 22:59:19 +0530

    Row material allocation page

---
+++ src/pages/Event/RawMaterialAllocationPage/grossary_items/constant.jsx
@@ -0,0 +1,108 @@
+export const data = [
+  { 
+    sr_no: 1, 
+    row_material: 'GHEE', 
+    qty: '4.500 kg', 
+    final_qty: '4.500', 
+    unit: 'KiloGram',
+    agency: '',
+    place: 'At Venue',
+    total_price: 2295.0,
+  },
+  { 
+    sr_no: 2, 
+    row_material: 'TATA SALT ( NAMAK )', 
+    qty: '5.500 Kg', 
+    final_qty: 5.500, 
+    unit: 'KiloGram',
+    agency: '',
+    place: 'At Venue',
+    total_price: 110.0,
+  },
+  { 
+    sr_no: 3, 
+    row_material: 'MAIDA', 
+    qty: '16.500 Kg', 
+    final_qty: 16.500, 
+    unit: 'KiloGram',
+    agency: 'HARDIK BHAI GAS',
+    place: 'At Venue',
+    total_price: 462,
+  },
+  { 
+    sr_no: 4, 
+    row_material: 'SUGAR (SHAKKAR)', 
+    qty: '6 Kg', 
+    final_qty: 6, 
+    unit: 'KiloGram',
+    agency: '',
+    place: 'At Venue',
+    total_price: 210,
+  },
+  { 
+    sr_no: 5, 
+    row_material: 'AJINO MOTO', 
+    qty: '750 g', 
+    final_qty: 750, 
+    unit: 'Gram',
+    agency: '',
+    place: 'At Venue',
+    total_price: 90,
+  },
+  { 
+    sr_no: 6, 
+    row_material: 'OIL', 
+    qty: '16 L', 
+    final_qty: 16, 
+    unit: 'Liter',
+    agency: 'HARDIK BHAI GAS',
+    place: 'At Venue',
+    total_price: 3200,
+  },
+  { 
+    sr_no: 7, 
+    row_material: 'COCONUT MILK TIN', 
+    qty: '10 NOS', 
+    final_qty: 10, 
+    unit: 'NOS',
+    agency: '',
+    place: 'At Venue',
+    total_price: 690,
+  },
+];
+
+
+export const columns = [
+  {
+    accessorKey: "sr_no",
+    header: "#",
+  },
+  {
+    accessorKey: "row_material",
+    header: "Row Material",
+  },
+  {
+    accessorKey: "qty",
+    header: "Qty",
+  },
+  {
+    accessorKey: "final_qty",
+    header: "Final Qty",
+  },
+  {
+    accessorKey: "unit",
+    header: "Unit",
+  },
+  {
+    accessorKey: "agency",
+    header: "Agency",
+  },
+  {
+    accessorKey: "place",
+    header: "Place",
+  },
+  {
+    accessorKey: "total_price",
+    header: "Total Price",
+  },
+];
--- src/pages/Event/RawMaterialAllocationPage/index.jsx
+++ src/pages/Event/RawMaterialAllocationPage/index.jsx
@@ -1,11 +1,56 @@
 import { Fragment } from "react";
 import { Container } from "@/components/container";
 import { Breadcrumbs } from "@/layouts/demo1/breadcrumbs/Breadcrumbs";
+import TabComponent from "@/components/tab/TabComponent";
+// import GrossaryItems from "@components/event/grossary_items/GrossaryItems";
+import GrossaryItems from "./grossary_items/GrossaryItems";
 import useStyles from "./style";
 
 const RawMaterialAllocationPage = () => {
   const classes = useStyles();
 
+  const tabs = [
+    {
+      value: "grossary_items",
+      label: <>Grossary Items</>,
+      children: <GrossaryItems />,
+    },
+    {
+      value: "vegetables",
+      label: 'Vegetables',
+      children: 'Vegetables',
+    },
+    {
+      value: "dairy_items",
+      label: 'Dairy Items',
+      children: 'Dairy Items',
+    },
+    {
+      value: "ice_department",
+      label: 'Ice Department',
+      children: 'Ice Department',
+    },
+    {
+      value: "mineral_water",
+      label: 'Mineral Water',
+      children: 'Mineral Water',
+    },
+    {
+      value: "ready_made_order",
+      label: 'Ready Made Order',
+      children: 'Ready Made Order',
+    },
+    {
+      value: "gas_batla_item",
+      label: 'GAS BATLA ITEM',
+      children: 'GAS BATLA ITEM',
+    },
+    {
+      value: "staff_salairy",
+      label: 'STAFF SALAIRY',
+      children: 'STAFF SALAIRY',
+    },
+  ];
   return (
     <Fragment>
       <Container>
@@ -13,11 +58,7 @@ const RawMaterialAllocationPage = () => {
         <div className="gap-2 pb-2 mb-3">
           <Breadcrumbs items={[{ title: "Raw Material Allocation" }]} />
         </div>
-        <div
-          className={`grid grid-cols-11 lg:grid-cols-11 border rounded-lg gap-4 p-4 ${classes.customStyle}`}
-        >
-          Raw Material Allocation
-        </div>
+         <TabComponent tabs={tabs} />
       </Container>
     </Fragment>
   );
