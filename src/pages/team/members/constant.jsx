78e21496c669ea44bc7d1cfb0438fd79012342b2
kaushik-kboy <shree.kaushik96@gmail.com>
2025-08-28 23:22:23 +0530

    Team and setting inside customize module

--- src/pages/team/members/constant.jsx
+++ src/pages/team/members/constant.jsx
@@ -1,6 +1,7 @@
 import { DataGridColumnHeader } from "@/components";
 import { Link } from "react-router-dom";
-import { Tooltip } from "antd";
+import { underConstruction } from "@/underConstruction";
+import { Tooltip, Popconfirm } from "antd";
 export const columns = [
   {
     accessorKey: "user",
@@ -40,14 +41,15 @@ export const columns = [
       return (
         <div className="flex items-center justify-center gap-1">
           <Tooltip title="View">
-            <Link to="/companydetails">
+            {/* <Link to="/companydetails"> */}
               <button
                 className="btn btn-sm btn-icon btn-clear text-success"
                 title="View"
+                onClick={() => cell.row.original.handleModalOpen()}
               >
                 <i className="ki-filled ki-eye"></i>
               </button>
-            </Link>
+            {/* </Link> */}
           </Tooltip>
           <Tooltip title="Edit">
             <button
@@ -58,14 +60,23 @@ export const columns = [
               <i className="ki-filled ki-notepad-edit"></i>
             </button>
           </Tooltip>
-          <Tooltip title="Delete">
-            <button
-              className="btn btn-sm btn-icon btn-clear text-danger"
-              title="Delete"
+          <Popconfirm
+              title="Are you sure to delete this item?"
+              onConfirm={() => underConstruction()
+              }
+              onCancel={() => console.log('Cancelled')}
+              okText="Yes"
+              cancelText="No"
             >
-              <i className="ki-filled ki-trash"></i>
-            </button>
-          </Tooltip>
+            <Tooltip title="Delete">
+              <button
+                className="btn btn-sm btn-icon btn-clear text-danger"
+                title="Delete"
+              >
+                <i className="ki-filled ki-trash"></i>
+              </button>
+            </Tooltip>
+            </Popconfirm>
         </div>
       );
     },
