bc13dde08c02411e224b331771a44caf676a4a9e
Tarun068 <tarunmaheshwari055@gmail.com>
2025-08-31 21:06:13 +0530

    changes in menu and all member api done

--- src/pages/master/all-menbers/index.jsx
+++ src/pages/master/all-menbers/index.jsx
@@ -10,7 +10,7 @@ import useStyle from "./style";
 
 import { Link } from "react-router-dom";
 import { underConstruction } from "@/underConstruction";
-
+import { FetchAllUser } from "@/services/apiServices";
 import AddMember from "@/partials/modals/add-member/AddMember";
 
 const AllMemberMaster = () => {
@@ -22,7 +22,45 @@ const AllMemberMaster = () => {
   const handleModalOpen = () => {
     setIsModalOpen(true);
   };
+  useEffect(() => {
+    Fetchalluser();
+  }, []);
 
+  let userData = JSON.parse(localStorage.getItem("userData"));
+  let Id = userData.id;
+  const Fetchalluser = () => {
+    FetchAllUser(Id)
+      .then((res) => {
+        const formatted = res.data.data["User Details"].map((cust, index) => ({
+          sr_no: index + 1,
+          first_name: cust.firstName,
+          last_name: cust.lastName,
+          country: cust.userBasicDetails.country.name,
+          whatsapp: cust.contactNo,
+          role: cust.userBasicDetails.role.name,
+          email: cust.email,
+          task_access: cust.userBasicDetails.isTaskAccess,
+          leave_attendence_access:
+            cust.userBasicDetails.isAttendanceLeaveAccess,
+          eventid: cust.id,
+        }));
+
+        setTableData(formatted);
+      })
+      .catch((error) => {
+        console.error("Error deleting customer:", error);
+      });
+  };
+  const DeleteUser = (id) => {
+    DeleteCategoryId(id)
+      .then((res) => {
+        Fetchalluser();
+        res.data?.msg && successMsgPopup(res.data.msg);
+      })
+      .catch((error) => {
+        console.error("Error deleting Event type:", error);
+      });
+  };
   const responseFormate = () => {
     const data = defaultData.map((item) => {
       return {
