0b16e1de54b525cf63a46bb7ede2dda175022f50
shreetarun2025-star <shree.tarun2025@gmail.com>
2025-09-06 01:17:36 -0400

    Revert "Config resolved"

--- src/container/EventStepsContainer/FunctionDetails/style.js
+++ src/container/EventStepsContainer/FunctionDetails/style.js
@@ -1,34 +1,7 @@
 import { makeStyles } from "@mui/styles";
 const useStyles = makeStyles({
   customStyle: {
-    "& .select__grp": {
-      "& .sg__inner": {
-        borderRadius: "6px",
-        "& .ant-select": {
-          height: "38px",
-          border: "0",
-          height: "38px",
-          paddingInlineStart: "0",
-          "& .ant-select-selector": {
-            border: "0",
-          },
-        },
-        "& .formGrpCommon": {
-          flex: "1",
-          "& .ant-select": {
-            height: "38px",
-            "& .ant-select-selector": {
-              border: "0",
-            },
-          },
-        },
-        "& > .sga__btn": {
-          maxWidth: "32px",
-          flex: "0 0 32px",
-          height: "32px",
-        },
-      },
-    },
+    // Add your custom styles here
   },
 });
 export default useStyles;
