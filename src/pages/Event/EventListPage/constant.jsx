a8bd00c59c7eff6e7e0feec84ba73ef24fad4959
Tarun068 <tarunmaheshwari055@gmail.com>
2025-09-07 00:22:52 +0530

    MENU Report is done

--- src/pages/Event/EventListPage/constant.jsx
+++ src/pages/Event/EventListPage/constant.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { underConstruction } from "@/underConstruction";
 import { useEffect, useRef, useState } from "react";
 
-export const columns = (onDelete, viewEvent) => [
+export const columns = (onDelete, viewEvent, openMenuReport) => [
   {
     accessorKey: "sr_no",
     header: "#",
@@ -139,6 +139,17 @@ export const columns = (onDelete, viewEvent) => [
               onClick={underConstruction}
             >
               <i className="ki-filled ki-grid  text-info"></i>
+            </button>
+            {/* </Link> */}
+          </Tooltip>
+          <Tooltip title="Menu Report">
+            {/* <Link to="/menu-allocation"> */}
+            <button
+              className="btn btn-sm btn-icon btn-clear"
+              title="Menu Report"
+              onClick={() => openMenuReport(row.original.eventid)}
+            >
+              <i className="ki-filled ki-notepad text-gray-500"></i>
             </button>
             {/* </Link> */}
           </Tooltip>
